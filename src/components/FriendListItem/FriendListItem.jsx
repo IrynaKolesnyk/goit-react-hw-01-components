0e57a0254918c512d1c63deb8407eaf42b042b17
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-07-19 17:17:03 +0200

    fixes friendsListItem

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,11 +4,8 @@ import styles from './FriendsListItem.module.css';
 const FriendListItem = ({ friend }) => {
     return (
         <li className={styles.item}>
-            {friend.isOnline ? (
-                <span className={styles.online}>{friend.isOnline}</span>
-            ) : (
-                <span className={styles.offline}>{friend.isOnline}</span>
-            )}
+            <span className={friend.isOnline ? styles.online : styles.offline}>{friend.isOnline}</span>
+            
             <img className={styles.avatar} src={friend.avatar} alt={friend.name} width="48" />
             <p className={styles.name}>{friend.name}</p>
         </li>
